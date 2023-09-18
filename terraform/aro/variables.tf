0a6407a2844e2d42d3c49bec1220ff22041150c3
raffaelespazzoli <raffaele.spazzoli@gmail.com>
2023-09-18 14:05:47 -0400

    f

--- terraform/aro/variables.tf
+++ terraform/aro/variables.tf
@@ -7,7 +7,7 @@ variable "location" {
   type = string
 }
 
-variable resource_group_name {
+variable "resource_group_name" {
   type = string
 }
 
@@ -22,18 +22,18 @@ variable "resource_prefix" {
 }
 
 
-variable domain {
+variable "domain" {
   type = string
   nullable = true
   default = ""
 }
 
-variable fips_validated_modules {
+variable "fips_validated_modules" {
   type = bool
   default = false
 }
 
-variable pull_secret {
+variable "pull_secret" {
   type = string
   sensitive = true
   nullable = true
@@ -42,67 +42,67 @@ variable pull_secret {
 
 
 
-variable pod_cidr {
+variable "pod_cidr" {
   type = string
   default = "10.128.0.0/14"
 }
 
-variable service_cidr {
+variable "service_cidr" {
   type = string
   default = "172.30.0.0/16"
 }
 
 
 
-variable clientId {
+variable "clientId" {
   type = string
   sensitive = true
 }
 
-variable clientSecret {
+variable "clientSecret" {
   type = string
   sensitive = true
 }
 
 
 
-variable master_node_vm_size {
+variable "master_node_vm_size" {
   type = string
   default = "Standard_D8s_v3"
 }
 
-variable master_subnet_id {
+variable "master_subnet_id" {
   type = string
 }
 
-variable master_encryption_at_host {
+variable "master_encryption_at_host" {
   type = string
   default = "Disabled"
 }
 
 
 
-variable worker_profile_name {
+variable "worker_profile_name" {
   type = string
   default = "worker"
 }
 
-variable worker_node_vm_size {
+variable "worker_node_vm_size" {
   type = string
   default = "Standard_D4s_v3"
 }
 
 
-variable worker_node_vm_disk_size {
+variable "worker_node_vm_disk_size" {
   type = number
   default = 128
 }
 
-variable worker_subnet_id {
+variable "worker_subnet_id" {
   type = string
 }
 
-variable worker_encryption_at_host {
+variable "worker_encryption_at_host" {
   type = string
   default = "Disabled"
 }
@@ -114,19 +114,19 @@ variable "worker_node_count" {
 
 
 
-variable api_server_visibility {
+variable "api_server_visibility" {
   type = string
   default = "Private"
 }
 
 
 
-variable ingress_profile_name {
+variable "ingress_profile_name" {
   type =string
   default = "default"
 }
 
-variable ingress_visibility {
+variable "ingress_visibility" {
   type = string
   default = "Private"
 }
