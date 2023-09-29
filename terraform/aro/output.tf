55ab56912e46b1fa6f38563d4b1d6835016e2ada
raffaelespazzoli <raffaele.spazzoli@gmail.com>
2023-09-29 15:41:58 -0400

    f

--- terraform/aro/output.tf
+++ terraform/aro/output.tf
@@ -1,12 +1,17 @@
-output "apiURL" {
-  value = jsondecode(azapi_resource.aro_cluster.body).properties.apiserverProfile.url
+output "properties" {
+  value = jsondecode(azapi_resource.aro_cluster.body).properties
   sensitive = true
 }
 
-output "consoleURL" {
-  value = jsondecode(azapi_resource.aro_cluster.body).properties.consoleProfile.url
-  sensitive = true
-}
+# output "apiURL" {
+#   value = jsondecode(azapi_resource.aro_cluster.body).properties.apiserverProfile.url
+#   sensitive = true
+# }
+
+# output "consoleURL" {
+#   value = jsondecode(azapi_resource.aro_cluster.body).properties.consoleProfile.url
+#   sensitive = true
+# }
 
 output "kubeconfig" {
   value = base64decode(jsondecode(azapi_resource_action.kubeconfig.output).kubeconfig)
