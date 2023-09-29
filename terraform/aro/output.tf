f167534db3ed56b403278d179aaeba9836860c22
raffaelespazzoli <raffaele.spazzoli@gmail.com>
2023-09-29 08:36:01 -0400

    f

--- terraform/aro/output.tf
+++ terraform/aro/output.tf
@@ -1,9 +1,11 @@
 output "apiURL" {
   value = azapi_resource.aro_cluster.body
+  sensitive = false
 }
 
 output "consoleURL" {
   value = azapi_resource.aro_cluster.response_export_values
+  sensitive = false
 }
 
 output "adminUsername" {
